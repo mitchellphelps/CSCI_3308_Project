b08446164949a65a7e9f7f6eaa574e8a23293fe6
Tristan Hanna <tristan.hanna@colorado.edu>
2020-03-26 16:47:52 -0600

    more firebase

--- Code and Components/Full_Software/js/titlescreen.js
+++ Code and Components/Full_Software/js/titlescreen.js
@@ -7,8 +7,8 @@ function openLogModal(){
 }*/
 
 var database = firebase.firestore();
-var docRef = database.collection("Adds");
-
+var docRef = database.collection("users");
+/*
 function show(){
     var nameShow = document.getElementById("nameShow");
     var placeShow = document.getElementById("placeShow");
@@ -18,49 +18,25 @@ function show(){
         placeShow.innerHTML = "";
         
         querySnapshot.forEach(function(doc){
-            var name = doc.data().name;
-            var location = doc.data().location;
+            var username = doc.data().username;
+            var highscore = doc.data().highscore;
             
             userNameShow.innerHTML += '<p>' + name + '</p>';
             highScoreShow.innerHTML += '<p>' + location + '</p>';
         });
     });
-}
+}*/
 
-function sign(){
+function signUp(){
     //getting information given
-	var newUser = document.getElementById("user").value;
-	var newEmail = document.getElementById("email").value;
-
-    if(document.getElementById("avayes").checked==true){
-		newAvalanche = "yes";
-	}
-	else if(document.getElementById("avano").checked==true){
-			newAvalanche = "no";
-		}
-    if(document.getElementById("trigyes").checked==true){
-		newTrigger = "yes";
-	}
-	else if(document.getElementById("trigno").checked==true){
-			newTrigger = "no";
-		}
-    if(document.getElementById("caughtyes").checked==true){
-		newCaught = "yes";
-	}
-	else if(document.getElementById("caughtno").checked==true){
-			newCaught = "no";
-		}
+	var newUser = document.getElementById("username").value;
+	var newPassword = document.getElementById("psw").value;
 	
 	//adding new document to collection with input values
-	database.collection("Adds").add({
-		name: newUser,
-        email: newEmail,
-        phone: newPhone,
-        location: newPlace,
-        description: newDescribe,
-        avalanche: newAvalanche,
-        trigger: newTrigger,
-        victims: newCaught
+	database.collection("users").add({
+		username: newUser,
+        password: newPassword,
+        highscore: 0
 	})
 	.then(function(docRef) {
 		console.log("Document written with ID: ", docRef.id);
@@ -69,11 +45,6 @@ function sign(){
 		console.error("Error adding document: ", error);
 	});
 }
-
-var submit=document.getElementById("the-button");
-submit.addEventListener("click", sign);
-
-show();
 
 function openSignModal() {       
     //var user = document.getElementById("username"); not needed yet
@@ -169,9 +140,7 @@ function enableButton(letter, capital, number, length, match) {
 
 
 function onClickFunction() {
-    //TODO: check if username is available and store new user with password, default settings,etc
-    var username = document.getElementById("username");
-    alert("Welcome to Runner, " + username + "!!!");
+    signUp();
 }
 
 function onLogInFunction() {
